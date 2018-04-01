445f274fbde1eaca73f1091c9c6cbaff2368932c
nicklegr <addr.for+github@gmail.com>
2018-04-01 23:38:35 +0900

    Practice A: ローカルで通った。サーバでRuntime Error

---
+++ practice/A.rb
@@ -0,0 +1,131 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    pp(*arg)
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    puts(*arg)
+  end
+end
+
+def parr(arr)
+  puts arr.join(" ")
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+class Integer
+  def popcount32
+    bits = self
+    bits = (bits & 0x55555555) + (bits >>  1 & 0x55555555)
+    bits = (bits & 0x33333333) + (bits >>  2 & 0x33333333)
+    bits = (bits & 0x0f0f0f0f) + (bits >>  4 & 0x0f0f0f0f)
+    bits = (bits & 0x00ff00ff) + (bits >>  8 & 0x00ff00ff)
+    return (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff)
+  end
+
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
+
+# main
+STDOUT.sync = true
+
+t_start = Time.now
+
+cases = readline().to_i
+
+(1 .. cases).each do |case_index|
+  # write your code here
+
+  a, b = ris
+  n = ri
+
+  low = a + 1
+  hi = b
+  guess = (low + hi) / 2
+
+  success = false
+  n.times do
+    # answer
+    puts guess
+
+    # response
+    res = rs
+    case res
+    when "CORRECT"
+      success = true
+      break
+    when "TOO_SMALL"
+      low = guess
+    when "TOO_BIG"
+      hi = guess
+    when "WRONG_ANSWER"
+      exit 1
+    end
+
+    guess = (low + hi) / 2
+  end
+
+  exit(1) unless success
+
+  # progress
+  trigger = 
+    if cases >= 10
+      case_index % (cases / 10) == 0
+    else
+      true
+    end
+
+  if trigger
+    STDERR.puts("case #{case_index} / #{cases}, time: #{Time.now - t_start} s")
+  end
+end
